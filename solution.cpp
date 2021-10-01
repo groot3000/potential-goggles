e79035041dce7acf819028aaf9c1e624c9b4a12f
shivam30000 <91734057+shivam30000@users.noreply.github.com>
2021-10-01 17:20:38 +0530

    3

--- solution.cpp
+++ solution.cpp
@@ -13,15 +13,7 @@ int32_t main()
         int fre[N]={0};
         int n;     cin>>n;
         int a[n];
-        assert(n<=100000);
-        assert(n>=1);
-        for(int i=0;i<n;++i){
-            cin>>a[i];
-            assert(a[i]<=1000000);
-            assert(a[i]>0);
-        }
-        sort(a,a+n);
-        int s=0;
+       
         for(int i=0;i<n;i++)
         {
             int d=fre[a[i]];
@@ -33,6 +25,5 @@ int32_t main()
         cout<<s<<"\n";
     }
     return 0;
-    // change 1
-    // change 2
+  // change 3
 }
