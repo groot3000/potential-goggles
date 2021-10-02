a4561911f2b736cb6e8bc3d077ae594c382ca035
dungeonn <91797558+dungeonn@users.noreply.github.com>
2021-10-02 22:39:41 +0530

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -9,28 +9,7 @@ int32_t main()
     int t;   cin>>t;
     assert(t<=10);
     assert(t>=1);
-    while(t--){
-        int fre[N]={0};
-        int n;     cin>>n;
-        int a[n];
-        assert(n<=100000);
-        assert(n>=1);
-        for(int i=0;i<n;++i){
-            cin>>a[i];
-            assert(a[i]<=1000000);
-            assert(a[i]>0);
-        }
-        sort(a,a+n);
-        int s=0;
-        for(int i=0;i<n;i++)
-        {
-            int d=fre[a[i]];
-            int u=(d*(d-1))/2;
-            s+=u;
-            for(int j=a[i]+a[i];j<N;j+=a[i])
-                fre[j]++;
-        }
-        cout<<s<<"\n";
+    
     }
     return 0;
 }
